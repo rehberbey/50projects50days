5afa90713501c70de6c56c9e642efe299e421e64
rehberbey <58523422+rehberbey@users.noreply.github.com>
2022-09-11 19:59:04 +0300

    initial commit

---
+++ split-landing-page/dist/js/index.js
@@ -0,0 +1,19 @@
+const splitLeft = document.querySelector("section.split.left");
+const splitRight = document.querySelector("section.split.right");
+if (!(splitLeft instanceof HTMLElement && splitRight instanceof HTMLElement)) {
+    throw new Error("split ögeleri bulunamadı!");
+}
+splitLeft.addEventListener("mouseenter", () => {
+    splitRight.classList.add("shrink");
+});
+splitLeft.addEventListener("mouseleave", () => {
+    splitRight.classList.remove("shrink");
+});
+splitRight.addEventListener("mouseenter", () => {
+    splitLeft.classList.add("shrink");
+});
+splitRight.addEventListener("mouseleave", () => {
+    splitLeft.classList.remove("shrink");
+});
+export {};
+//# sourceMappingURL=index.js.map
