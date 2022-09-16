80eeddfc3fafdc6b36503706640e48cc67cd46f8
rehberbey <58523422+rehberbey@users.noreply.github.com>
2022-09-16 19:10:33 +0300

    initial commit

---
+++ form-input-wave/dist/js/index.js
@@ -0,0 +1,10 @@
+const labels = document.querySelectorAll("#login-form > form > .form-control > label");
+const delay = 50;
+labels.forEach((label) => {
+    label.innerHTML = label.innerText
+        .split("")
+        .map((letter, index) => `<span style="transition-delay: ${index * delay}ms">${letter}</span>`)
+        .join("");
+});
+export {};
+//# sourceMappingURL=index.js.map
