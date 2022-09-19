b9f26b44a8f98ef5b6ad15b2bc459e296bc09eb2
rehberbey <58523422+rehberbey@users.noreply.github.com>
2022-09-19 15:14:13 +0300

    initial commit

---
+++ event-keycodes/src/public/js/index.ts
@@ -0,0 +1,25 @@
+const main = document.querySelector("main");
+const propsEvent = new Set<keyof KeyboardEvent>(["key", "keyCode", "code"]);
+
+if (!main) {
+    throw new Error("Main ögesi bulunamadı!");
+}
+
+addEventListener("keydown", (event) => {
+    main.innerHTML = "";
+
+    for (const prop of propsEvent) {
+        let propValue = event[prop];
+
+        propValue = propValue === " " ? "Space" : propValue;
+
+        main.insertAdjacentHTML(
+            "beforeend",
+            `<div class="key">${String(
+                propValue
+            )}<small>event.${prop}</small></div>`
+        );
+    }
+});
+
+export {};
